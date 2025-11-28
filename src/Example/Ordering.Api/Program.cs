5e0a175a57559c0082ec68a4620cb0428828b93e
Ratchapol Anantawat <ratchapol.anantawat@agoda.com>
2025-11-28 21:34:45 +0700

    feat: first release

--- src/Example/Ordering.Api/Program.cs
+++ src/Example/Ordering.Api/Program.cs
@@ -25,10 +25,14 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    
+    // for swagger-ui
     app.UseSwaggerUI(options =>
     {
         options.SwaggerEndpoint("/openapi/v1.json", "v1");
     });
+    
+    // for scalar
     app.MapScalarApiReference();
 }
 
